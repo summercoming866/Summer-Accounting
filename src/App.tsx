29c8357ec2f05478c894eaa052fce8d4f747b6e4
lfy <438467132@qq.com>
2021-03-16 09:55:06 +0800

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import Nav from './components/Nav'
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+const Wrapper = styled.div`
+/* border:1px solid red; */
+min-height:100vh;
+display:flex;
+flex-direction:column;
+`
+const Main = styled.div`
+/* border:1px solid green;  */
+flex-grow:1;
+overflow:auto;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Wrapper>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+          </Switch>
+        </Main>
+        <Nav />
+          
+        
+
+      </Wrapper>
+    </Router>
   );
 }
+function NoMatch() {
+  return (
+    <div>页面不存在，地址错误</div>
+  )
+}
+function Statistics() {
+  return <h2>统计页</h2>;
+}
 
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
 export default App;
