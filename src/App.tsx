2df0c0df2eef5e1090ecb3adeef468390747f779
lfy <438467132@qq.com>
2021-03-16 23:10:19 +0800

    Money.tsx-TagSection-UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Tags from './views/Tags'
 import Money from './views/Money'
 import Statistics from './views/Statistics'
 import NoMatch from './views/NoMatch'
+import styled from 'styled-components'
 
 import {
   HashRouter as Router,
@@ -12,27 +13,30 @@ import {
   Redirect
 } from "react-router-dom";
 
-
+const AppWrapper = styled.div`
+  color:#333;
+`
 function App() {
   return (
-    <Router>
-
-      <Switch>
-        <Route path="/tags">
-          <Tags />
-        </Route>
-        <Route path="/money">
-          <Money />
-        </Route>
-        <Route path="/statistics">
-          <Statistics />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-        <Redirect exact from="/" to="/money" />
-      </Switch>
-    </Router >
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+        </Switch>
+      </Router >
+    </AppWrapper>
   );
 }
 
