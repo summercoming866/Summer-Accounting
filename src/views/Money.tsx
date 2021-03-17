4b7840aa525c0029a42713a4fac6cc12c032b039
lfy <438467132@qq.com>
2021-03-17 12:56:56 +0800

    money ui finished

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,8 +2,18 @@ import Layout from '../components/Layout'
 import React from 'react'
 import styled from 'styled-components'
 
+const MyLayout=styled(Layout)`
+    display:flex;
+    flex-direction:column;
+`
+
 const TagSection = styled.section
     `
+    flex-grow:1;
+    display:flex;
+    flex-direction:column;
+    justify-content:flex-end;
+    align-items:flex-start;
     background:#fff; 
     padding:12px 16px;
     >ol{
@@ -77,11 +87,66 @@ const CategorySection = styled.section
 `
 const NumberPadSection = styled.section
     `
+    display:flex;
+    flex-direction:column;
+    >.output{
+        font-size:36px;
+        background:white;
+        line-height:72px;
+        text-align:right;
+        padding:0 16px;
+        box-shadow:inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+        inset 0 -5px 5px -5px rgba(0,0,0,0.25)
+    }
+    >.pad{
+        >button{
+            font-size:18px;
+            width:25%;
+            height:64px;
+            float:left;
+            &.ok{
+                height:128px;
+                float:right;
+            }
+            &.zero{
+                width:50%;
+            }
+            &:nth-child(1){
+                background:#f2f2f2;
+              }
+              &:nth-child(2),
+              &:nth-child(5) {
+                background:#E0E0E0;
+              }
+              &:nth-child(3),
+              &:nth-child(6),
+              &:nth-child(9) {
+                background:#D3D3D3;
+              }
+              &:nth-child(4),
+              &:nth-child(7),
+              &:nth-child(10) {
+                background:#C1C1C1;
+              }
+              &:nth-child(8),
+              &:nth-child(11),
+              &:nth-child(13) {
+                background:#B8B8B8;
+              }
+              &:nth-child(12) {
+                background:#9A9A9A;
+              }
+              &:nth-child(14) {
+                background:#A9A9A9;
+              }
+        }
+
+    }
 
 `
 function Money() {
     return (
-        <Layout>
+        <MyLayout>
             <TagSection>
                 <ol>
                     <li>衣</li>
@@ -105,28 +170,30 @@ function Money() {
                 </ul>
             </CategorySection>
             <NumberPadSection>
-                <div>100</div>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
+                <div className="output">100</div>
+                <div className="pad clearfix">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>删除</button>
 
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>清空</button>
 
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>OK</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button className="ok">OK</button>
 
 
-                <button>0</button>
-                <button>.</button>
+                    <button className="zero">0</button>
+                    <button className="dot">.</button>
+                </div>
 
             </NumberPadSection>
-        </Layout>
+        </MyLayout>
     );
 }
 export default Money
