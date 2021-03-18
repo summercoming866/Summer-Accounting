02a6de8f2db19ba70305dc598e4c53d2e2b7f8c7
lfy <438467132@qq.com>
2021-03-18 11:08:11 +0800

    numberpadsection fund finished

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -60,7 +60,15 @@ const Wrapper = styled.section
 
 `
 const NumberPadSection: React.FC = () => {
-    const [output, setOutput] = useState('0')
+    const [output, _setOutput] = useState('0')
+    const setOutput=(output:string)=>{
+        if(output.length>16){
+            output=output.slice(0,16)
+        }else if(output.length===0){
+            output='0'
+        }
+        _setOutput(output)
+    }
     const onClickButtonWrapper = (e: React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent
         if (text === null) { return }
@@ -75,7 +83,7 @@ const NumberPadSection: React.FC = () => {
             case '7':
             case '8':
             case '9':
-            case '.':
+            
                 if (output === '0') {
                     setOutput(text)
                 } else {
@@ -83,12 +91,24 @@ const NumberPadSection: React.FC = () => {
                 }
                 break;
 
-
+            
+            case '.':
+                if(output.indexOf('.')>=0){
+                    return;
+                }else{
+                    setOutput(output+'.')
+                }
+                break;
             case '删除':
+                if(output.length===1){
+                    setOutput('0')
+                }else{
+                    setOutput(output.slice(0,-1))
+                }
                 console.log('删除')
                 break;
             case '清空':
-                console.log('清空')
+                setOutput('')
                 break;
             case 'OK':
                 console.log('ok')
