63a9b01c9bd37bc0ca7f0825f286a250b03fe558
lfy <438467132@qq.com>
2021-04-01 12:12:23 +0800

    title

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yarn build &&
+git init && 
+git add . &&
+git commit -m "deploy" &&
+git remote add origin &&
+git remote add origin git@github.com:summercoming866/SummerAccounting-website.git &&
+git push -u origin main -f 
+cd -
