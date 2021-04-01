5943567f902ce1dcaf7415bb894981f15d0b4acb
lfy <438467132@qq.com>
2021-04-01 15:40:27 +0800

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,7 @@ git init &&
 git add . &&
 git commit -m "deploy" &&
 git remote add origin git@github.com:summercoming866/SummerAccounting-website.git &&
+# git remote add origin git@gitee.com:summercoming866/SummerAccounting-website.git &&
+git branch -m main &&
 git push -u origin main -f 
 cd ..
